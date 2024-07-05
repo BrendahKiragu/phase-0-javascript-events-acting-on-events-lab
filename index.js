3fbd4cc7619777bef77ad3721fa9e0a40f0c8fe9
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-07-05 22:56:15 +0300

    creates a move to right event

--- index.js
+++ index.js
@@ -12,3 +12,13 @@ function moveDodgerLeft() {
   }
 }
 
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+  
+  // Taking 400 as the maximum width of the container
+  if (left < 400 - dodger.offsetWidth) {
+    dodger.style.left = `${left + 1}px`; // Increases the left value to move right
+  }
+}
+
